9f6c7d53794bf66546032d389f7a0584d9bb31af
Ivan Kozik <ivan@ludios.org>
2024-10-23 23:55:42 +0000

    GM_addStyle -> GM.addStyle

--- hide_hacker_news_stories.user.js
+++ hide_hacker_news_stories.user.js
@@ -4,10 +4,10 @@
 // @author      ludios
 // @license     AGPL-3.0; https://www.gnu.org/licenses/agpl-3.0.en.html
 // @grant       none
-// @version     1.0.1
+// @version     1.0.3
 // @match       https://news.ycombinator.com/*
 // @match       https://news.ycombinator.com./*
-// @grant       GM_addStyle
+// @grant       GM.addStyle
 // @run-at      document-start
 // ==/UserScript==
 
@@ -16,7 +16,7 @@ const loc = window.location.href;
 const HOMEPAGE_RE = /^https:\/\/news\.ycombinator\.com\.?\/(news)?($|[?&])/;
 
 if (HOMEPAGE_RE.test(loc)) {
-  GM_addStyle(`
+  GM.addStyle(`
     body > center > table#hnmain > tbody > tr > td > table[border="0"][cellpadding="0"][cellspacing="0"]:has(tbody > tr.athing) {
       display: none;
     }
